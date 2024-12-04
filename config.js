1d3b5933805c5144443f4e9466963c599e2010b3
hikingguide <lena.oppliger@bbz-cfp.ch>
2024-12-04 15:23:00 +0000

    Stand 04.12 um 16:22 Uhr

--- config.js
+++ config.js
@@ -39,14 +39,55 @@ export function createApp(dbconfig) {
 
   app.locals.pool = pool;
 
+  //Register
+
   app.get("/register", (req, res) => {
     res.render("register");
   });
+
+  app.post("/register", function (req, res) {
+    var password = bcrypt.hashSync(req.body.password, 10);
+    pool.query(
+      "INSERT INTO users (name, username, password) VALUES ($1, $2, $3)",
+      [req.body.name, req.body.username, password],
+      (error, result) => {
+        if (error) {
+          console.log(error);
+        }
+        res.redirect("/login");
+      }
+    );
+  });
+
+  app.get("/login", function (req, res) {
+    res.render("login");
+  });
+
+  app.post("/login", function (req, res) {
+    pool.query(
+      "SELECT * FROM users WHERE username = $1",
+      [req.body.username],
+      (error, result) => {
+        if (error) {
+          console.log(error);
+        }
+        if (bcrypt.compareSync(req.body.password, result.rows[0].password)) {
+          req.session.userid = result.rows[0].id;
+          res.redirect("/");
+        } else {
+          res.redirect("/login");
+        }
+      }
+    );
+  });
+
+  ////
 
   app.get("/event_formular", function (req, res) {
     res.render("event_formular");
   });
 
+  //Upload Formular
   app.post("/gallery", upload.single("image"), async function (req, res) {
     const date = new Date();
     await app.locals.pool.query(
